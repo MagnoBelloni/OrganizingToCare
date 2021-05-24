14ff090397b39bfb2d3e3eed6989f35377476ad2
Leoads99 <leosouza-3799@hotmail.com>
2021-05-24 18:14:32 -0300

    CRUD de Medicamentos

--- organizingtocare/app/controllers/medicamento.py
+++ organizingtocare/app/controllers/medicamento.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from app.models.medicamento import Medicamento
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask import  flash, get_flashed_messages, json, jsonify
 
 
 @app.route("/medicamento")
@@ -11,14 +12,49 @@ def index_medicamentos():
 
 @app.route("/medicamento/novo")
 def novo_medicamento():
+    if request.method =='POST':
+        # crio um objeto cliente com os dados do formulário 
+        medicamento = Medicamento(
+        request.form['nome_medicamento'],
+        request.form['data_validade'],
+        request.form['quantidade'],
+        request.form['peso'])
+        db.session.add(medicamento)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template("medicamento/novo.html")
+
+    if request.form['nome_medicamento'] == '':
+        flash('Nome do medicamento é obrigatório!')
+
+    if request.form['data_validade'] == '':
+        flash('A data de validade é obrigatória!')
+
+    if request.form['quantidade'] == '':
+        flash('A quantidade é obrigatória!')
+
+    if len(get_flashed_messages()) > 0:
+        return render_template("medicamento/novo.html")
+
 
 
 @app.route("/medicamento/editar/<int:id>")
 def editar_medicamento():
-    return render_template("medicamento/editar.html")
+    # select from 
+    medicamento = Medicamento.query.get(id)
+    if request.method == 'POST':
+        medicamento.nome_medicamento = request.form['nome_medicamento']
+        medicamento.data_validade = request.form['data_validade']
+        medicamento.quantidade = request.form['quantidade']
+        medicamento.peso = request.form['peso']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("medicamento/editar.html", medicamento = medicamento)     
 
 
 @app.route("/medicamento/excluir/<int:id>")
-def excluir_medicamento():
-    return redirect("/medicamento")
+def excluir_medicamento(id):
+    medicamento = Medicamento.query.get(id)
+    db.session.delete(medicamento)
+    db.session.commit()
+    return redirect(url_for("index"))
