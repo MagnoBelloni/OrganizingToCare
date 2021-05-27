8afc606191cb7b7f532552318a19e0a85917bfaa
Leoads99 <leosouza-3799@hotmail.com>
2021-05-27 14:32:16 -0300

    Implementado datetime formatada, colunas e botões de excluir e editar ocultos quando o tipo de usuário é diferente de admin

--- organizingtocare/app/models/medicamento.py
+++ organizingtocare/app/models/medicamento.py
@@ -1,12 +1,15 @@
 from datetime import datetime, date
 from app import db
 
+dataCriacao = datetime.now()
+dataCriacaoString = dataCriacao.strftime('%d/%m/%Y %H:%M:%S')
+dataCriacaoFormatada = datetime.strptime(dataCriacaoString,'%d/%m/%Y %H:%M:%S')
 
 class Medicamento(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
     dataVencimento = db.Column(db.DateTime, nullable=False)
-    dataCriacao = db.Column(db.DateTime, default=datetime.now())
+    dataCriacao = db.Column(db.DateTime, default=dataCriacaoFormatada)
     quantidade = db.Column(db.String(10), nullable=False)
     peso = db.Column(db.String(50), nullable=False)
 
