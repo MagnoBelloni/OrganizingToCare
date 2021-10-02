65b6aabc86367e7d41aaf9d5180a4af004073987
Magno Belloni <bellonidesouza6@gmail.com>
2021-10-02 11:44:56 -0300

    Implementação da lógica da página Home

--- organizingtocare/app/controllers/home.py
+++ organizingtocare/app/controllers/home.py
@@ -1,18 +1,22 @@
 from app import app
 from flask import render_template
-from app.models.medicamento import Medicamento
+from app.models.medicamento_estoque import MedicamentoEstoque
 from datetime import datetime, timedelta
 
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
-    # filtro_vencimento_em_dias = datetime.today() + timedelta(days=15)
+    filtro_vencimento_em_dias = datetime.today() + timedelta(days=20)
 
-    # medicamentos_para_vencer = Medicamento.query.filter(
-    #     Medicamento.dataVencimento <= filtro_vencimento_em_dias).all()
+    medicamentos_para_vencer = MedicamentoEstoque.query.filter(
+        MedicamentoEstoque.dataVencimento <= filtro_vencimento_em_dias
+    ).order_by(MedicamentoEstoque.dataVencimento.asc()).all()
 
-    # for medicamento in medicamentos_para_vencer:
-    #     medicamento.vencido = medicamento.dataVencimento < datetime.today()
+    medicamentos_baixo_estoque = MedicamentoEstoque.query.filter(
+        MedicamentoEstoque.quantidadeInicial / 3 >= MedicamentoEstoque.quantidade
+    ).order_by(MedicamentoEstoque.quantidade.asc()).all()
 
-    # return render_template("home.html", medicamentos=medicamentos_para_vencer)
+    for medicamento in medicamentos_para_vencer:
+        medicamento.vencido = medicamento.dataVencimento < datetime.today()
+
+    return render_template("home.html", medicamentos_para_vencer=medicamentos_para_vencer, medicamentos_baixo_estoque=medicamentos_baixo_estoque)
