b8bc3405317fd18633d1d10b2ed49d0ad0ef6d00
Magno Belloni <39836481+MagnoBelloni@users.noreply.github.com>
2021-10-02 11:45:29 -0300

    Feature/novos campos (#10)
    * Inclusão dos campos lote e nome da pessoa que trouxe o medicamento para a tabela medicamento_estoque
    
    * Implementação da lógica da página Home

--- organizingtocare/app/models/medicamento_estoque.py
+++ organizingtocare/app/models/medicamento_estoque.py
@@ -7,13 +7,17 @@ class MedicamentoEstoque(db.Model):
     dataVencimento = db.Column(db.DateTime, nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
     quantidadeInicial = db.Column(db.Integer, nullable=False)
+    lote = db.Column(db.String, nullable=False)
+    nomePessoaTrouxeMedicamento = db.Column(db.String, nullable=False)
 
     medicamento_estoque_registro_association = db.relationship('MedicamentoEstoqueRegistro', order_by='MedicamentoEstoqueRegistro.dataRegistro.desc()')
 
-    def __init__(self, medicamentoPacienteId, dataVencimento, quantidade, quantidadeInicial):
+    def __init__(self, medicamentoPacienteId, dataVencimento, quantidade, quantidadeInicial, lote, nomePessoaTrouxeMedicamento):
         self.medicamentoPacienteId = medicamentoPacienteId
         self.dataVencimento = dataVencimento
         self.quantidade = quantidade
         self.quantidadeInicial = quantidadeInicial
+        self.lote = lote
+        self.nomePessoaTrouxeMedicamento = nomePessoaTrouxeMedicamento
 
     MedicamentoPaciente = db.relationship('MedicamentoPaciente', backref=db.backref("medicamento_estoque"))
